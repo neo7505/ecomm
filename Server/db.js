e52d830ac3dfb1a988e6cdaf485fd92d9294cedc
Chitrankar <chitrankarramanandi86@gmail.com>
2024-04-19 15:48:28 +0530

    first commit

---
+++ Server/db.js
@@ -0,0 +1,19 @@
+const moongoose = require("mongoose");
+
+module.exports =()=>{
+    const connectionParams = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+    try {
+        moongoose.connect(process.env.DB,connectionParams);
+        console.log("Connected to database successfully")
+    } catch (error) {
+        console.log(error)
+        console.log("Could not connect to database")
+    }
+}
+
+
+// username: chitrankarramanandi86
+// password: HNO9p7lvUHCBCOLN
